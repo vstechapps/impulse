7d2bce76b0b828b54f3c1bd64743c642a90b652e
vstechapps vvsk <vstechapps@gmail.com>
2024-09-14 04:43:20 +0000

    updated

---
+++ src/app/datatable/datatable.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-datatable',
+  standalone: true,
+  imports: [],
+  templateUrl: './datatable.component.html',
+  styleUrl: './datatable.component.less'
+})
+export class DatatableComponent {
+  @Input
+  Table
+}
