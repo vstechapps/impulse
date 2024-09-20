ba3fa92b974ab95ad153c9f63e6c234f5c058afe
vstechapps vvsk <vstechapps@gmail.com>
2024-09-20 05:58:22 +0000

    updated

--- src/app/datatable/datatable.model.ts
+++ src/app/datatable/datatable.model.ts
@@ -1,3 +1,12 @@
 export interface DataTable {
-    
+    data:any[];
+    cols:string[];
+    actions:Action[];
+}
+
+export interface Action{
+    id:string;
+    text?:string;
+    class?:string[];
+    icon?:string;
 }
